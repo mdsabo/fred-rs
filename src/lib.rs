47ebe91b3556928e0661cb0288ddcf33dd983494
slabo101 <slab.o101@gmail.com>
2020-02-21 12:27:19 -0500

    api checklist

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![crate_name = "fred_rs"]
+#![doc(html_root_url = "https://docs.rs/fred-rs/0.1.0")]
 
 //! **fred-rs** is a simple interface for accessing the Federal Reserve Bank of St. Louis's FRED API.
 //! 
